abb796d822abf8a9b402c1f1d95a4483c78d577b
tianjx98 <973970940@qq.com>
2022-03-03 23:58:00 +0800

    引入静态页面

--- src/main/java/cn/tianjx98/views/about/AboutView.java
+++ src/main/java/cn/tianjx98/views/about/AboutView.java
@@ -1,6 +1,9 @@
 package cn.tianjx98.views.about;
 
+import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.IFrame;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -8,24 +11,31 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
 import cn.tianjx98.views.MainLayout;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 @PageTitle("About")
 @Route(value = "about", layout = MainLayout.class)
-public class AboutView extends VerticalLayout {
+public class AboutView extends IFrame {
 
     public AboutView() {
-        setSpacing(false);
+        super("home/index.html");
+        setHeightFull();
+        setWidthFull();
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        //setSpacing(false);
+        //
+        //Image img = new Image("images/empty-plant.png", "placeholder plant");
+        //img.setWidth("200px");
+        //add(img);
+        //
+        //add(new H2("This place intentionally left empty"));
+        //add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        //
+        //setSizeFull();
+        //setJustifyContentMode(JustifyContentMode.CENTER);
+        //setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        //getStyle().set("text-align", "center");
     }
 
 }
