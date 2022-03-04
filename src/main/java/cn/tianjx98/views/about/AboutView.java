dc4d846c128cebb45d9445ebc395536d7e56254c
田俊雄 <v-tianjunxiong@oppo.com>
2022-03-04 18:39:42 +0800

    配置文件替换为yml

--- src/main/java/cn/tianjx98/views/about/AboutView.java
+++ src/main/java/cn/tianjx98/views/about/AboutView.java
@@ -1,41 +1,46 @@
 package cn.tianjx98.views.about;
 
-import com.vaadin.flow.component.Html;
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.IFrame;
+import com.github.appreciated.card.ClickableCard;
+import com.github.appreciated.card.content.IconItem;
 import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
-import cn.tianjx98.views.MainLayout;
+import com.vaadin.flow.router.Route;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import cn.tianjx98.views.MainLayout;
 
 @PageTitle("About")
 @Route(value = "about", layout = MainLayout.class)
-public class AboutView extends IFrame {
+public class AboutView extends VerticalLayout {
 
     public AboutView() {
-        super("home/index.html");
-        setHeightFull();
-        setWidthFull();
+        // setSpacing(false);
+        //
+        // Image img = new Image("images/empty-plant.png", "placeholder plant");
+        // img.setWidth("200px");
+        // add(img);
+        //
+        // add(new H2("This place intentionally left empty"));
+        // add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        //
+        // setSizeFull();
+        // setJustifyContentMode(JustifyContentMode.CENTER);
+        // setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        // getStyle().set("text-align", "center");
 
-        //setSpacing(false);
-        //
-        //Image img = new Image("images/empty-plant.png", "placeholder plant");
-        //img.setWidth("200px");
-        //add(img);
-        //
-        //add(new H2("This place intentionally left empty"));
-        //add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-        //
-        //setSizeFull();
-        //setJustifyContentMode(JustifyContentMode.CENTER);
-        //setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        //getStyle().set("text-align", "center");
+
+        clickableCardExample("images/empty-plant.png", "123","1221312");
+    }
+
+    public void clickableCardExample(String imagePath, String title, String description) {
+        Image img = new Image(imagePath, title);
+        img.setWidth("50px");
+        img.setHeight("50px");
+        ClickableCard card = new ClickableCard(componentEvent -> Notification.show("A ClickableCard was clicked!"),
+                        new IconItem(img, title, description));
+        card.setWidth("100%");
+        add(card);
     }
 
 }
