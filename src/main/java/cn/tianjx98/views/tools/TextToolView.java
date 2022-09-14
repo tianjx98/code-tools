6c644142a95a930521738c5adb65c999080eafcb
tianjx98 <973970940@qq.com>
2022-09-14 14:35:01 +0800

    修复import错误

--- src/main/java/cn/tianjx98/views/tools/TextToolView.java
+++ src/main/java/cn/tianjx98/views/tools/TextToolView.java
@@ -1,11 +1,15 @@
 package cn.tianjx98.views.tools;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.AnchorTarget;
+import com.vaadin.flow.component.html.Footer;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
 
 import cn.tianjx98.aop.annotations.Menu;
 import cn.tianjx98.components.tabs.Tabs;
