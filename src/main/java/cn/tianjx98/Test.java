648f69cb7c0ba477f6dfc898d815d119202fd272
田俊雄 <v-tianjunxiong@oppo.com>
2022-12-20 15:45:57 +0800

    添加chatGPT工具

--- src/main/java/cn/tianjx98/Test.java
+++ src/main/java/cn/tianjx98/Test.java
@@ -1,27 +1,41 @@
 package cn.tianjx98;
 
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.extra.mail.MailUtil;
-import cn.hutool.http.HttpUtil;
-import org.springframework.http.*;
-import org.springframework.util.MultiValueMap;
-import org.springframework.util.StreamUtils;
-import org.springframework.web.client.RestTemplate;
-
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.StreamUtils;
+
+import com.alibaba.fastjson.JSON;
+
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
 
 public class Test {
     public static void main(String[] args) {
-
+        final HashMap<String, Object> param = new HashMap<>();
+        param.put("prompt", "cron表达式每三分钟执行一次怎么写");
+        param.put("max_tokens", 1000);
+        param.put("model", "text-davinci-003");
+        final HttpResponse response = HttpUtil.createPost("https://api.openai.com/v1/completions")
+                        .header("Content-Type", "application/json")
+                        .header("Authorization", "Bearer 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")
+                        .body(JSON.toJSONString(param)).execute();
+        System.out.println(response.body());
     }
 
-    private RequestEntity createRequestEntity(HttpServletRequest request, String url) throws URISyntaxException, IOException {
+    private RequestEntity createRequestEntity(HttpServletRequest request, String url)
+                    throws URISyntaxException, IOException {
         String method = request.getMethod();
         HttpMethod httpMethod = HttpMethod.resolve(method);
         MultiValueMap<String, String> headers = parseRequestHeader(request);
---
+++ src/main/java/cn/tianjx98/views/tools/ChatGPTView.java
@@ -0,0 +1,75 @@
+package cn.tianjx98.views.tools;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.util.StringUtils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.KeyPressEvent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import cn.hutool.http.HttpUtil;
+import cn.tianjx98.aop.annotations.Tab;
+import cn.tianjx98.views.MainLayout;
+import lombok.extern.log4j.Log4j2;
+
+@PageTitle("ChatGPT")
+@Route(value = "chat-gpt", layout = MainLayout.class)
+@Tab(value = "ChatGPT", group = "textTool", order = 5)
+@Log4j2
+public class ChatGPTView extends VerticalLayout {
+    TextField question;
+    TextArea answer;
+
+
+    public ChatGPTView() {
+        setHeightFull();
+        setWidthFull();
+        final VerticalLayout col1 = new VerticalLayout();
+        add(col1);
+        question = new TextField("问题");
+        question.setHeight("80px");
+        question.setWidth("700PX");
+        answer = new TextArea("答案");
+        answer.setHeight("550px");
+        answer.setWidth("700PX");
+        col1.add(question);
+        col1.add(answer);
+
+        question.addKeyPressListener(Key.ENTER, new ComponentEventListener<KeyPressEvent>() {
+            @Override
+            public void onComponentEvent(KeyPressEvent event) {
+                if (StringUtils.hasText(question.getValue())) {
+                    System.out.println(question.getValue());
+                    final HashMap<String, Object> param = new HashMap<>();
+                    param.put("prompt", question.getValue());
+                    param.put("max_tokens", 1000);
+                    param.put("model", "text-davinci-003");
+                    answer.setValue(getResult(param));
+                }
+            }
+        });
+    }
+
+    private String getResult(Map<String, Object> param) {
+        try {
+            final String body = HttpUtil.createPost("https://api.openai.com/v1/completions")
+                            .header("Content-Type", "application/json")
+                            .header("Authorization", "Bearer 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")
+                            .body(JSON.toJSONString(param)).executeAsync().body();
+
+            return ((JSONObject) JSON.parseObject(body).getJSONArray("choices").get(0)).getString("text").trim();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+}
