fde1040c59da9583f3df4fbf9045f718811ed49b
田俊雄 <v-tianjunxiong@oppo.com>
2023-03-29 11:22:39 +0800

    添加计算器文档链接

--- src/main/java/cn/tianjx98/views/tools/CalculatorView.java
+++ src/main/java/cn/tianjx98/views/tools/CalculatorView.java
@@ -6,6 +6,7 @@ import cn.tianjx98.aop.annotations.Tab;
 import cn.tianjx98.views.MainLayout;
 import com.googlecode.aviator.AviatorEvaluator;
 import com.googlecode.aviator.Options;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
@@ -46,6 +47,7 @@ public class CalculatorView extends VerticalLayout {
         col1.setHeightFull();
         col1.setWidthFull();
         page.add(col1);
+        col1.add(new Span("参考文档: https://www.yuque.com/boyan-avfmj/aviatorscript/lvabnw"));
         sqlInput = new TextArea("表达式");
         sqlInput.setHeightFull();
         sqlInput.setWidthFull();
